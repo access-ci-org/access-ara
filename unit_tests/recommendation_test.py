f592d54ef9fb91453f5e9925ee4d5b22fee6742a
aiden-hamade <133402650+aiden-hamade@users.noreply.github.com>
2024-02-07 14:44:01 -0500

    fixed unit tests
    Unit tests now accurately correspond to new scoring logic.

--- unit_tests/recommendation_test.py
+++ unit_tests/recommendation_test.py
@@ -1,5 +1,5 @@
 import pytest
-from app.logic.recommendation import calculate_points, classify_rp_storage, calculate_score_rf, calculate_score_jc, calculate_score_software, get_recommendations
+from app.logic.recommendation import calculate_points, classify_rp_storage, calculate_score_rf, calculate_score_software, get_recommendations
 
 
 """
@@ -54,53 +54,45 @@ def test_calculate_score_rf():
                                                                  'Stampede-2': {'score': 1, 'reasons': ['Computer Science']},
                                                                  'Expanse': {'score': 1, 'reasons': ['Computer Science']}}
 
-jobClassList = ['Machine Learning', 'Deep Learning']
-
-def test_calculate_score_jc():
-    assert calculate_score_jc(jobClassList, scoreboard) == {'Jetstream2': {'score': 1, 'reasons': ['Civil Engineering']},
-                                                            'Bridges-2': {'score': 3, 'reasons': ['Civil Engineering', 'Computer Science', 'Machine Learning']},
-                                                            'Stampede-2': {'score': 1, 'reasons': ['Computer Science']},
-                                                            'Expanse': {'score': 1, 'reasons': ['Computer Science']},
-                                                            'Delta': {'score': 2, 'reasons': ['Deep Learning', 'Machine Learning']},
-                                                            'DARWIN': {'score': 1, 'reasons': ['Machine Learning']}}
-    
 softwareList = ['GCC', 'TensorFlow']
 
 def test_calculate_score_software():
     assert calculate_score_software(softwareList, scoreboard) == {'Jetstream2': {'score': 1, 'reasons': ['Civil Engineering']},
-                                                                'Bridges-2': {'score': 5, 'reasons': ['Civil Engineering', 'Computer Science', 'Machine Learning', 'gcc', 'tensorflow']},
-                                                                'Stampede-2': {'score': 2, 'reasons': ['Computer Science', 'gcc']},
-                                                                'Expanse': {'score': 2, 'reasons': ['Computer Science', 'gcc']},
-                                                                'Delta': {'score': 3, 'reasons': ['Deep Learning', 'Machine Learning', 'gcc']},
-                                                                'DARWIN': {'score': 2, 'reasons': ['Machine Learning', 'tensorflow']},
-                                                                'ACES': {'score': 1, 'reasons': ['tensorflow']},
-                                                                'FASTER': {'score': 1, 'reasons': ['gcc']},
-                                                                'OOKAMI': {'score': 1, 'reasons': ['gcc']},
-                                                                'Rockfish': {'score': 2, 'reasons': ['gcc', 'tensorflow']}}
+                                                                'Bridges-2': {'score': 22, 'reasons': ['Civil Engineering', 'Computer Science', 'gcc', 'tensorflow']},
+                                                                'Stampede-2': {'score': 11, 'reasons': ['Computer Science', 'gcc']},
+                                                                'Expanse': {'score': 11, 'reasons': ['Computer Science', 'gcc']},
+                                                                'Delta': {'score': 10, 'reasons': ['gcc']},
+                                                                'DARWIN': {'score': 10, 'reasons': ['tensorflow']},
+                                                                'ACES': {'score': 20, 'reasons': ['gcc', 'tensorflow']},
+                                                                'FASTER': {'score': 10, 'reasons': ['gcc']},
+                                                                'OOKAMI': {'score': 10, 'reasons': ['gcc']},
+                                                                'Rockfish': {'score': 20, 'reasons': ['gcc', 'tensorflow']}}
 
 formData = {'hpc-use': '1', 'access-familiarity': '1', 'used-hpc': ['Bridges-2', 'Delta', 'FASTER', 'KyRIC'],
             'hpc-experience': '4+ years', 'gui-needed': '1', 'used-gui': ['Open OnDemand'],
             'storage': '1', 'num-files': '', 'long-term-storage': 'less-than-1', 'temp-storage': '1-10',
-            'memory': 'more-than-512', 'software': 'tensorflow,gcc', 'graphics': '1', 'cpu-gpu-parallel': '1',
+            'memory': 'more-than-512', 'software': 'tensorflow,gcc', 'graphics': '1', 'gpu': '1',
             'job-run': '0', 'vm': '0', 'research-field': 'Civil Engineering,Computer Science', 'add-field-tags': '',
             'add-software-tags': '', 'job-class': 'Machine Learning,Deep Learning', 'add-job-tags': ''}
 
 badFormData = {'Hello': '123'}
 
 def test_get_recommendations():
-    assert get_recommendations(formData) == {'Bridges-2': {'score': 10, 'reasons': ['User Experience', 'Open OnDemand', 'Civil Engineering', 'Computer Science', 'Machine Learning', 'Long Term Storage', 'gcc', 'tensorflow', 'Graphics']},
-                                                  'Delta': {'score': 9, 'reasons': ['User Experience', 'Deep Learning', 'Machine Learning', 'Long Term Storage', 'Temporary Storage', '2000 GB Memory', 'gcc', 'Graphics']},
-                                                  'FASTER': {'score': 6, 'reasons': ['User Experience', 'Open OnDemand', 'Temporary Storage', 'gcc', 'Graphics']},
-                                                  'KyRIC': {'score': 6, 'reasons': ['User Experience', 'Long Term Storage', 'Temporary Storage', '3000 GB Memory', 'Graphics']},
-                                                  'Expanse': {'score': 6, 'reasons': ['Open OnDemand', 'Computer Science', '2000 GB Memory', 'gcc', 'Graphics']},
-                                                  'Anvil': {'score': 2, 'reasons': ['Open OnDemand', '1000 GB Memory']},
-                                                  'ACES': {'score': 5, 'reasons': ['Open OnDemand', 'Temporary Storage', 'tensorflow', 'Graphics']},
-                                                  'Jetstream2': {'score': 3, 'reasons': ['Civil Engineering', 'Long Term Storage', '1024 GB Memory']},
-                                                  'Stampede-2': {'score': 4, 'reasons': ['Computer Science', 'gcc', 'Graphics']},
-                                                  'DARWIN': {'score': 7, 'reasons': ['Machine Learning', 'Temporary Storage', '1024 GB Memory', '2048 GB Memory', 'tensorflow', 'Graphics']},
-                                                  'Open Science Grid': {'score': 1, 'reasons': ['Long Term Storage']},
-                                                  'Open Storage Network': {'score': 1, 'reasons': ['Long Term Storage']},
-                                                  'Rockfish': {'score': 4, 'reasons': ['Scratch Storage', '1500 GB Memory', 'gcc', 'tensorflow']},
-                                                  'OOKAMI': {'score': 1, 'reasons': ['gcc']}}
+    assert get_recommendations(formData) == {'Bridges-2': {'score': 29, 'reasons': ['User Experience', 'Open OnDemand', 'Civil Engineering', 'Computer Science', 'Long Term Storage', 'gcc', 'tensorflow', 'Graphics', 'GPU']},
+                                             'Delta': {'score': 17, 'reasons': ['User Experience', 'Long Term Storage', 'Temporary Storage', '2000 GB Memory', 'gcc', 'Graphics', 'GPU']},
+                                             'FASTER': {'score': 17, 'reasons': ['User Experience', 'Open OnDemand', 'Temporary Storage', 'gcc', 'Graphics', 'GPU']},
+                                             'KyRIC': {'score': 6, 'reasons': ['User Experience', 'Long Term Storage', 'Temporary Storage', '3000 GB Memory', 'Graphics']},
+                                             'Expanse': {'score': 16, 'reasons': ['Open OnDemand', 'Computer Science', '2000 GB Memory', 'gcc', 'Graphics', 'GPU']},
+                                             'Anvil': {'score': 3, 'reasons': ['Open OnDemand', '1000 GB Memory', 'GPU']},
+                                             'ACES': {'score': 25, 'reasons': ['Open OnDemand', 'Temporary Storage', 'gcc', 'tensorflow', 'Graphics', 'GPU']},
+                                             'Jetstream2': {'score': 4, 'reasons': ['Civil Engineering', 'Long Term Storage', '1024 GB Memory', 'GPU']},
+                                             'Stampede-2': {'score': 13, 'reasons': ['Computer Science', 'gcc', 'Graphics']},
+                                             'Open Science Grid': {'score': 2, 'reasons': ['Long Term Storage', 'GPU']},
+                                             'Open Storage Network': {'score': 1, 'reasons': ['Long Term Storage']},
+                                             'DARWIN': {'score': 16, 'reasons': ['Scratch Storage', '1024 GB Memory', '2048 GB Memory', 'tensorflow', 'Graphics', 'GPU']},
+                                             'Rockfish': {'score': 23, 'reasons': ['Scratch Storage', '1500 GB Memory', 'gcc', 'tensorflow', 'GPU']},
+                                             'OOKAMI': {'score': 11, 'reasons': ['gcc', 'GPU']}}
+
+
     with pytest.raises(AttributeError):
         get_recommendations(badFormData)
