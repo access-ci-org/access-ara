5735cae3c05c88d9c9cf5d33fc604cabea6b9daf
Sandesh-l <sandesh.lamichhane1@gmail.com>
2024-02-28 15:30:24 -0500

    removed jobclass files

--- app/models/rpJobClass.py
+++
@@ -1,9 +0,0 @@
-from . import *
-from .jobClass import JobClass
-from .rps import RPS
-
-class RpJobClass(BaseModel):
-    id = PrimaryKeyField()
-    rp = ForeignKeyField(RPS)
-    job_class = ForeignKeyField(JobClass, backref="rps_with_JobClass")
-    suitability = IntegerField(default=0)
